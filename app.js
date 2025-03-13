1020947313471eacd1e6754140cefb7bccb13404
roopa26 <roopashree65@gmail.com>
2025-03-13 14:50:25 +0530

    playing around route

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
 
+app.get("/a(b)?c",(req, res)=>{
+    res.send("Hi from abc page")
+});
+
+app.get("/user/:userId",(req, res)=>{
+    const {userId} = req.params
+    console.log(userId)
+    res.send(`Hi from user ${userId}`)
+});
 
 app.use("/hello",(req, res)=>{
     res.send("Hi from hello page")
@@ -8,9 +17,9 @@ app.use("/hello",(req, res)=>{
 app.use("/test",(req, res)=>{
     res.send("Hi from test page")
 });
-app.use("/",(req, res)=>{
-    res.send("Hi from dashbord page")
-});
+// app.use("/",(req, res)=>{
+//     res.send("Hi from dashbord page")
+// });
 
 app.listen(3000,()=>{
     console.log("server connected successfully at port 7777")
